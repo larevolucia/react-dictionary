8d6aa9be4828abf055f7c6d9028a86bc29da6c4e
larevolucia <reis.lucia@gmail.com>
2024-06-12 11:47:12 +0200

    imported google fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./Index.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Dictionary</h1>
+        <h1>word finder</h1>
       </header>
     </div>
   );
