4cc02f3dc3374d71962ccc128b1e8d570f441e5d
larevolucia <reis.lucia@gmail.com>
2024-06-12 13:23:26 +0200

    added axios and called API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}...`);
+    // alert(`Searching for ${keyword}...`);
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiURL).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
