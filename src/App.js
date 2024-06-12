b55d9ff5938344db780e95760e58a4cfbfd4f2a6
larevolucia <reis.lucia@gmail.com>
2024-06-12 12:38:10 +0200

    search bar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>word finder</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>word finder</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Lucia</footer>
+      </div>
     </div>
   );
 }
