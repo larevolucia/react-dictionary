713e80a72f36c5e744a18d89538cf93b045db80c
larevolucia <reis.lucia@gmail.com>
2024-06-17 16:03:21 +0200

    synonyms tags disabled if not in API catalogue

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  // console.log(props);
+  const [status, setStatus] = useState({});
+
+  useEffect(() => {
+    const fetchStatuses = async () => {
+      const newStatus = {};
+      for (let synonym of props.synonyms) {
+        newStatus[synonym] = "loading";
+        try {
+          let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${synonym}`;
+          await axios.get(apiURL);
+          newStatus[synonym] = "api-success"; // Apply CSS class for successful response
+        } catch (error) {
+          // console.error("Error fetching data:", error);
+          newStatus[synonym] = "api-error"; // Apply CSS class for error response
+        }
+      }
+      setStatus(newStatus);
+    };
+
+    if (props.synonyms.length > 0) {
+      fetchStatuses();
+    }
+  }, [props.synonyms]);
 
   if (props.synonyms.length !== 0) {
     return (
@@ -13,6 +36,8 @@ export default function Synonyms(props) {
               type="submit"
               value={synonym}
               onClick={props.keywordChange}
+              className={status[synonym] || ""}
+              disabled={status[synonym] === "api-error"}
             />
           </form>
         ))}
